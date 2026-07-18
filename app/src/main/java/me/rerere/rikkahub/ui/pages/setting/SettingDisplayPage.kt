e5161146a436357f45bc1282f32cea807ee4f525
smartworldarafath <smartworld.bd.710@gmail.com>
2026-07-18 17:49:09 +0600

    Fix imports and lock dav4jvm to verified JitPack version 3.0.2

--- app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingDisplayPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingDisplayPage.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import java.text.DateFormat
 import java.util.Date
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
