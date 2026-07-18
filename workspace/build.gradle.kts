926872e957125e9816b4f6f105773610d000db23
smartworldarafath <smartworld.bd.710@gmail.com>
2026-07-18 17:23:45 +0600

    Configure c++_shared STL for workspace native CMake build and push to main

--- workspace/build.gradle.kts
+++ workspace/build.gradle.kts
@@ -16,6 +16,7 @@ android {
         externalNativeBuild {
             cmake {
                 cppFlags += ""
+                arguments += "-DANDROID_STL=c++_shared"
             }
         }
     }
